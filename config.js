53549693841b2f3e0e4305b04de2153b10f5ef4f
Ryan Hoffmann <rhoff37@gmail.com>
2015-12-16 15:20:26 -0500

    Heroku Deploy Test

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
    'tokenSecret': 'goodnightsweetprince', // used for the json web token
    'cookieSecret': 'latersonthemenjay',
-   'database': 'mongodb://localhost/lines-db'
-   // 'database': 'mongodb://mumush:Macwinlin37@ds035573.mongolab.com:35573/lines-db' // Hosted on mongolab.com
+   // 'database': 'mongodb://localhost/lines-db'
+   'database': 'mongodb://mumush:Macwinlin37@ds035573.mongolab.com:35573/lines-db' // Hosted on mongolab.com
 };
